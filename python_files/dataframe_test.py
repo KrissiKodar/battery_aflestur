0ab6b1e690f8556efe98b80d95b49f5881c77c90
KrissiTekkari <dadasonk@gmail.com>
2023-04-25 14:35:55 +0000

    sql a godri leid, aetla ad laga space vesenid

--- python_files/dataframe_test.py
+++ python_files/dataframe_test.py
@@ -1,13 +1,13 @@
 import pandas as pd
 import pickle
 
-### save BQ4050 dataflash golden file ###
 with open('pkl_files\BQ4050_df_golden_file.pkl', 'rb') as file:
     # Dump the dictionary to the file
     data_df = pickle.load(file)
 
+with open('pkl_files\SBS_BQ4050.pkl', 'rb') as file:
+    # Dump the dictionary to the file
+    data_SBS = pickle.load(file)
 
-print(data_df)
 
-if "SUBCLAS" in data_df:
-    print("yes")
+print(data_SBS)
